a00c974d31cef357e5a2f4db8bae4996e84d00d5
Shibler0 <rischmartin@laposte.net>
2026-04-27 19:17:55 +0200

    fix(ui): new page show correctly

--- Frontend/assets/js/login.js
+++ Frontend/assets/js/login.js
@@ -1,18 +1,19 @@
-const tabs = document.querySelectorAll('.row-start p');
 const boxSignIn = document.querySelector('.box.active');
 const boxSignUp = document.querySelector('.sign-up');
 
-tabs.forEach((tab, index) => {
-    tab.addEventListener('click', () => {
-        document.querySelector('.row-start .active').classList.remove('active');
-        tab.classList.add('active');
+document.querySelectorAll('.row-start').forEach(rowStart => {
+    rowStart.querySelectorAll('p').forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            document.querySelectorAll('.row-start p').forEach(p => p.classList.remove('active'));
+            document.querySelectorAll(`.row-start p:nth-child(${index + 1})`).forEach(p => p.classList.add('active'));
 
-        if (index === 0) {
-            boxSignIn.style.display = 'block';
-            boxSignUp.style.display = 'none';
-        } else {
-            boxSignIn.style.display = 'none';
-            boxSignUp.style.display = 'block';
-        }
+            if (index === 0) {
+                boxSignIn.style.display = 'block';
+                boxSignUp.style.display = 'none';
+            } else {
+                boxSignIn.style.display = 'none';
+                boxSignUp.style.display = 'block';
+            }
+        });
     });
 });
